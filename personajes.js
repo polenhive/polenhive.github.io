afc1c7ef4772b5a6b0479bb3d5d648b29397927d
polenhive <polenhive@hotmail.com>
2026-05-23 10:59:03 -0600

    Update personajes.js

--- personajes.js
+++ personajes.js
@@ -1,9 +1,14 @@
 export const personajes = [
+
   {
     id: 'karla',
+
     nombre: 'Karla Pardo, La princesa de Nuevo Tigre',
+
     rol: 'Protagonista',
-    img: 'img/karla.jpg',
+
+    img: 'img/Karla.png',
+
     facts: [
       'Edad: 17',
       'Ocupación: Estudiante de la Preparatoria Altavira',
@@ -14,16 +19,29 @@ export const personajes = [
       'Interes: Música',
       'Relaciones: Heriberto Pardo (Padre) , Rufino Pardo (Medio Hermano), Máximo Pardo (Medio Hermano), Katarzyna Mucek (Madre), Diana Trejo (Mejor Amiga)'
     ],
+
     bio: `Su lugar favorito es su cuarto, porque ahí puede perderse por horas entre partituras y melodías. Toca toda clase de instrumentos desde que era niña y la música se ha convertido en parte de quien es: practica todos los días, compone pequeñas piezas y escucha géneros de todo tipo para encontrar inspiración. Sueña con convertirse en una artista reconocida.`,
+
     extra: `Su relación con Diana:
-Con Diana no siempre todo fue miel sobre hojuelas. A finales de su primer año de preparatoria, las dos quedaron nominadas en un concurso a la “Mejor Vestida”, y lo que empezó como competencia inocente pronto se convirtió en una rivalidad llena de tensión y ganas de demostrar quién merecía el título. Karla y Diana estaban decididas a ganar… pero al final, la corona terminó en manos de una tal Becky Valderrama.
-Y aunque parecía el tipo de momento que rompería cualquier posibilidad de amistad, pasó todo lo contrario. Después de perder, la rivalidad se apagó y entre ellas nació una conexión inesperada. Desde entonces, Diana se convirtió en la persona de confianza de Karla: la acompaña a todos lados, le da opiniones honestas aunque a veces no quiera escucharlas, y es quien la baja a la tierra cuando “se le sube lo Pardo”.`
+
+Con Diana no siempre todo fue miel sobre hojuelas. A finales de su primer año de preparatoria, las dos quedaron nominadas en un concurso a la “Mejor Vestida”, y lo que empezó como competencia inocente pronto se convirtió en una rivalidad llena de tensión y ganas de demostrar quién merecía el título.
+
+Karla y Diana estaban decididas a ganar… pero al final, la corona terminó en manos de una tal Becky Valderrama.
+
+Y aunque parecía el tipo de momento que rompería cualquier posibilidad de amistad, pasó todo lo contrario. Después de perder, la rivalidad se apagó y entre ellas nació una conexión inesperada.
+
+Desde entonces, Diana se convirtió en la persona de confianza de Karla: la acompaña a todos lados, le da opiniones honestas aunque a veces no quiera escucharlas, y es quien la baja a la tierra cuando “se le sube lo Pardo”.`
   },
+
   {
     id: 'diana',
+
     nombre: 'Diana Trejo',
+
     rol: 'Mejor amiga',
-    img: 'img/diana.jpg',
+
+    img: 'img/Diana.png',
+
     facts: [
       'Edad: 17',
       'Ocupación: Estudiante de la Preparatoria Altavira',
@@ -34,15 +52,29 @@ Y aunque parecía el tipo de momento que rompería cualquier posibilidad de amis
       'Interés: Moda',
       'Relaciones: Karla Pardo (Mejor amiga), Máximo Pardo (crush imposible), Familia Trejo (Padres)'
     ],
-    bio: `Diana siempre ha tenido buen ojo para la moda. Desde pequeña le gustaba combinar ropa, arreglar prendas viejas y dibujar vestidos en las esquinas de sus cuadernos. Sueña con algún día convertirse en una gran modista y tener su propia marca, aunque sabe que no será fácil. Sus padres son estrictos y esperan que continúe trabajando en el negocio familiar: un almacén de frutas que ha pasado de generación en generación.
-La escuela y su amistad con Karla se han convertido en un pequeño espacio de libertad donde puede ser ella misma. A diferencia de Karla, Diana suele ser más tranquila y aterrizada aun queno tiene problema en decir verdades incómodas cuando hace falta.
-Desde hace tiempo tiene un crush con Máximo Pardo, el medio hermano de Karla, cree que es muy buena disimulando pero en realidad se pone nerviosa cada vez que él aparece cerca.`
+
+    bio: `Diana siempre ha tenido buen ojo para la moda. Desde pequeña le gustaba combinar ropa, arreglar prendas viejas y dibujar vestidos en las esquinas de sus cuadernos.
+
+Sueña con algún día convertirse en una gran modista y tener su propia marca, aunque sabe que no será fácil.
+
+Sus padres son estrictos y esperan que continúe trabajando en el negocio familiar: un almacén de frutas que ha pasado de generación en generación.
+
+La escuela y su amistad con Karla se han convertido en un pequeño espacio de libertad donde puede ser ella misma.
+
+A diferencia de Karla, Diana suele ser más tranquila y aterrizada aunque no tiene problema en decir verdades incómodas cuando hace falta.
+
+Desde hace tiempo tiene un crush con Máximo Pardo. Cree que es muy buena disimulando pero en realidad se pone nerviosa cada vez que él aparece cerca.`
   },
+
   {
     id: 'maximo',
+
     nombre: 'Máximo Pardo',
+
     rol: 'Misterioso',
-    img: 'img/maximo.jpg',
+
+    img: 'img/Maximo.png',
+
     facts: [
       'Edad: 22',
       'Ocupación: Candidato a la presidencia municipal de Nuevo Tigre',
@@ -53,9 +85,26 @@ Desde hace tiempo tiene un crush con Máximo Pardo, el medio hermano de Karla, c
       'Intereses: fútbol, Redes sociales',
       'Relaciones: Heriberto Pardo (padre), Rufino Pardo (medio hermano), Karla Pardo (media hermana), Katarzyna Mucek (madrastra), Diana Trejo (mejor amiga de Karla)'
     ],
-    bio: `Máximo siempre quiso ser futbolista. Desde niño soñaba con debutar profesionalmente, pero su padre, Heriberto Pardo, le repetía que “eso es para la prole” y lo empujó a seguir la tradición familiar: la política. 
-Hoy es el candidato joven del momento: carismático, atractivo y experto en convertir cualquier aparición pública en contenido viral. Está más preocupado por encontrar el ángulo perfecto para un live que por memorizar el reglamento municipal. Habla en eslógan, posa en cada evento y parece vivir permanentemente en campaña.
-Gracias a su encanto en pantalla, acumula más de un millón de seguidores en Meowstagram. Mientras los medios más serios lo critican por su fama de party boy superficial, el mundo del espectáculo lo adora. Sale en revistas, aparece en fiestas exclusivas y siempre encuentra la manera de escapar de los escándalos con una sonrisa perfectamente ensayada.
-Pero detrás de toda esa imagen pública, Máximo vive atrapado entre expectativas que nunca eligió , aunque jamás lo admitiría en voz alta, su padre lo intimida más de lo que inspira. Frente a Heriberto, Máximo vuelve a sentirse como el niño al que le dijeron que sus sueños no eran suficientes.`
+
+    bio: `Máximo siempre quiso ser futbolista. Desde niño soñaba con debutar profesionalmente, pero su padre, Heriberto Pardo, le repetía que “eso es para la prole” y lo empujó a seguir la tradición familiar: la política.
+
+Hoy es el candidato joven del momento: carismático, atractivo y experto en convertir cualquier aparición pública en contenido viral.
+
+Está más preocupado por encontrar el ángulo perfecto para un live que por memorizar el reglamento municipal.
+
+Habla en eslóganes, posa en cada evento y parece vivir permanentemente en campaña.
+
+Gracias a su encanto en pantalla, acumula más de un millón de seguidores en Meowstagram.
+
+Mientras los medios más serios lo critican por su fama de party boy superficial, el mundo del espectáculo lo adora.
+
+Sale en revistas, aparece en fiestas exclusivas y siempre encuentra la manera de escapar de los escándalos con una sonrisa perfectamente ensayada.
+
+Pero detrás de toda esa imagen pública, Máximo vive atrapado entre expectativas que nunca eligió.
+
+Aunque jamás lo admitiría en voz alta, su padre lo intimida más de lo que inspira.
+
+Frente a Heriberto, Máximo vuelve a sentirse como el niño al que le dijeron que sus sueños no eran suficientes.`
   }
+
 ];
